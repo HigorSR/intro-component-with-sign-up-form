ef11a47c54c985c29a26deb3ac0f53fdac7a535b
Higor Rocha <higor_36rocha@hotmail.com>
2023-12-16 15:50:55 -0300

    First Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "Red": "hsl(0, 100%, 74%) ",
+        "Green": "hsl(154, 59%, 51 %)",
+        
+        /* Accent */
+        "Blue": "hsl(248, 32 %, 49 %)",
+
+        /* Neutral */
+        "Dark Blue": "hsl(249, 10 %, 26 %)",
+        "Grayish Blue": "hsl(246, 25 %, 77 %)",
+      }
     },
   },
   plugins: [],

ad86c9dd8b6e5a3cb1b16cf84f25e04029e749a6
Higor Rocha <higor_36rocha@hotmail.com>
2024-08-12 18:04:32 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,20 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        'mobile': "430px",
       },
-    },
-    colors: {
-      "Red": "hsl(0, 100%, 74%)",
-      "Green": "hsl(154, 59%, 51%)",
+      backgroundImage: {
+        'mobile': "url('/bg-intro-mobile.png')",
+        'desktop': "url('/bg-intro-desktop.png')",
+      },
+      colors: {
+        'Red': "hsl(0, 100%, 74%)",
+        'Green': "hsl(154, 59%, 51%)",
 
-      "Blue": "hsl(248, 32%, 49%)",
+        'Blue': "hsl(248, 32%, 49%)",
 
-      "Dark-Blue": "hsl(249, 10%, 26%)",
-      "Grayish-Blue": "hsl(246, 25%, 77%)",
+        "Dark-Blue": "hsl(249, 10%, 26%)",
+        "Grayish-Blue": "hsl(246, 25%, 77%)",
+      },
     },
   },
   plugins: [],

5027ffe39d2f0abff414a6cc2566047e3117f82e
Higor Rocha <higor_36rocha@hotmail.com>
2024-08-13 00:48:36 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,8 @@ import TestFree from "@/components/test-free";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center gap-14 bg-mobile bg-cover bg-no-repeat bg-center bg-Red pt-20 pb-5 md:flex-row md:gap-10 md:h-dvh sm:bg-desktop">
+    <main className="flex flex-col items-center justify-center gap-14 bg-mobile bg-cover bg-no-repeat bg-center bg-Red pt-20 pb-5 md:flex-row md:gap-10 md:h-dvh 
+    sm:bg-[url('../../public/bg-intro-desktop.png')]">
       <Header />
 
       <div className="flex flex-col gap-6 mx-6 xl:w-[500px]">
